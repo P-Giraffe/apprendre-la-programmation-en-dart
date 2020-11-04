6bce6cce8b1ed89389bd5e1f4f4461f3d1a75e23
Maxime Britto <maxime@purplegiraffe.fr>
2020-11-04 16:29:17 +0100

    18_3_Exercice__Calcul_de_moyenne_-_saisie_des_notes

--- bases-dart/8_s_entrainer/calcul_moyenne/app.dart
+++ bases-dart/8_s_entrainer/calcul_moyenne/app.dart
@@ -32,7 +32,32 @@ void main() {
   } while (choixUtilisateur != choixQuitter);
 }
 
-void ajouterNotes(Map<String, List<double>> listeMatieres) {}
+void ajouterNotes(Map<String, List<double>> listeMatieres) {
+  // 1 - Demander le nom de la matière
+  String nomMatiere;
+  do {
+    nomMatiere = readText("Quelle est la matière ?");
+  } while (nomMatiere.length <= 0);
+
+  // 2 - Combien de notes pour cette matière
+  int nombreNotes;
+  do {
+    nombreNotes = readInt("Combien de notes à saisir en $nomMatiere ?");
+  } while (nombreNotes < 0);
+
+  // 3 - Demander chacune des notes et les insérer dans listeMatieres
+  if (nombreNotes == 0) {
+    final listeNotesDeLaMatiere = listeMatieres[nomMatiere] ?? [];
+    for (var numeroNote = 1; numeroNote <= nombreNotes; numeroNote++) {
+      double note;
+      do {
+        note = readDouble("Entrez la note $numeroNote en $nomMatiere");
+      } while (note < 0 || note > 20);
+      listeNotesDeLaMatiere.add(note);
+    }
+    listeMatieres[nomMatiere] = listeNotesDeLaMatiere;
+  }
+}
 
 void afficherMoyennes(Map<String, List<double>> listeMatieres) {}
 
